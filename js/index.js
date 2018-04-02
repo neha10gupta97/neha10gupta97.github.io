cffd2ad90ea94d0f9ae8eb003df1870449cccb35
Neha Gupta <neha.gupta@voylla.com>
2018-04-02 18:24:08 +0530

    hover on header links

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 $('.header-links').hover(
-    function(){
-        $('.header-links').addClass("bold");
-    }
+  function(){
+    $('.header-links').addClass('bold');
+    }, function(){
+      $('.header-links').removeClass('bold');
+  });
 );
